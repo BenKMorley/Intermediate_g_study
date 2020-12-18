1430afba886170f684d490a8230d6dc5765a7d08
BenKMorley <bkm1n18@soton.ac.uk>
2020-12-18 16:31:09 +0000

    Added use of a virtual environment for python

--- run_mass_set.sh
+++ run_mass_set.sh
@@ -5,13 +5,21 @@ g=1
 L=16
 NOR = 10
 ALPHA = 0
-SAVE_FREQ = 
 
+# Don't bother saving at the moment as the runs are so cheap
+SAVE_FREQ = 99999999
 
-END = 
+END = 100000
+
 NODES = 1
 
+# Use my virtual environment for this project
+source /home/dc-kitc1/virtual_envs/Intermediate_g_env/bin/activate
+
 python3 mass_array.py $N $g $L
+
+# Leave this virtual environment
+deactivate
 
 X=`cat masses_to_run_temp.txt`
 
