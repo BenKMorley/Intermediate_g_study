19d34216ee0835b4bf061fe542d07da4b369b246
BenKMorley <bkm1n18@soton.ac.uk>
2021-01-08 11:36:00 +0000

    Testing read in

--- raw_to_MCMC.py
+++ raw_to_MCMC.py
@@ -8,6 +8,7 @@ def read_in_raw(N, g, L, m, base_dir="/rds/project/dirac_vol4/rds-dirac-dp099/co
     file_prefix = f"cosmhol-hbor-su2_L{L}_g{g}._m2-{m:.5f}_or{OR}"
 
     M2 = numpy.fromfile(f"{in_dir}/{file_prefix}_m2.0.dat")
+    M4 = numpy.fromfile(f"{in_dir}/{file_prefix}_m4.0.dat")
 
     pdb.set_trace()
 
