0fdfd1373aa2e1d4bc7add99ec9bc2ec41702499
BenKMorley <bkm1n18@soton.ac.uk>
2021-01-22 14:33:30 +0000

    Bug fix: 2 --> N for generality

--- Plot_Binder.py
+++ Plot_Binder.py
@@ -12,10 +12,15 @@ matplotlib.use('tkagg')
 
 
 def plot_Binder(N, g_s, L_s):
+    fig, ax = plt.subplots()
+    ax.set_xlabel(r'$\frac{m^2 - m_c^2}{g^2} x^\frac{1}{\nu}$')
+    ax.set_ylabel(r'$B(N, g, L)$')
+    markers = {16: 'v', 32: '<'}
+
     with h5py.File("MCMC_test.h5", "r") as f:
         for g in g_s:
             for L in L_s:
-                params = get_statistical_errors_central_fit(2)['params_central']
+                params = get_statistical_errors_central_fit(N)['params_central']
                 alpha = params[0]
                 beta = params[-2]
                 nu = params[-1]
@@ -42,14 +47,14 @@ def plot_Binder(N, g_s, L_s):
 
                 masses = numpy.array(masses)
 
-                plt.scatter(((m_crit - masses) / m_crit) * (g * L) ** (1 / nu), Binders)
-                plt.xlabel(r'$\frac{m_c^2 - m^2}{m_c^2} x^\frac{1}{\nu}$')
-                plt.ylabel(r'$B(N, g, L)$')
-                plt.show()
+                ax.scatter(((masses - m_crit) / g ** 2) * (g * L) ** (1 / nu), Binders, marker=markers[L], label=f'g={g}, L={L}')
+
+    plt.legend()
+    plt.show()
 
 
-g_s = [1]
+g_s = [1, 2, 4, 8, 16, 32]
 L_s = [16]
-N = 2
+N = 4
 
 plot_Binder(N, g_s, L_s)
