8417ce29084c7b6879be93037b7b35c913f1f69a
B.T.Kitching-Morley <bkm1n18@srv02876.soton.ac.uk>
2021-05-17 11:55:49 +0100

    bash script for looping through Binderanalysis.py

---
+++ Photon/run_loop_binder.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+NUM_THREADS=60
+NAME=Binderanalysis.py
+END_TASK_NO=1
+ID=0
+N=3
+
+cd ../Core
+
+G_S=(0.1 0.2 0.3 0.5 0.6)
+L_S=(8 16 32 48 64 96)
+B_S=(0.40 0.41 0.42 0.43 0.44 0.45 0.46)
+
+LEN_G=${#G_S[@]}
+LEN_L=${#L_S[@]}
+LEN_B=${#B_S[@]}
+END=$(( $LEN_G * $LEN_L * $LEN_B ))
+G_SIZE=$(( $END / $LEN_G ))
+M=$(( $LEN_G * $LEN_L ))
+
+
+while [ $ID -le $END ]; do
+
+  if [ $(( $RUNNING_PROCESSES )) -lt $(( $NUM_THREADS )) ]
+  then
+    G_INDEX=$(( ID/(LEN_B*LEN_L) ))
+    L_INDEX=$(( (ID/LEN_B)%LEN_L ))
+    B_INDEX=$(( ID%LEN_B ))
+    G=${G_S[$G_INDEX]}
+    L=${L_S[$L_INDEX]}
+    B=${B_S[$B_INDEX]}
+
+    echo about to run with ID=$ID G=$G L=$L B=$B
+
+    python3 $NAME $N $G $B $L &
+  fi
+
+  RUNNING_PROCESSES=$(ps -ef | grep $NAME | wc -l)
+  
+  echo RUNNING_PROCESSES : $RUNNING_PROCESSES
+
+  ID=$(($ID + 1))
+done
+
+
+# Now we need to wait for all of the processes to finsish
+while [ $RUNNING_PROCESSES -gt $END_TASK_NO ]; do
+  
+  sleep 0.5
+  
+  # RUNNING_PROCESSES=$(ps -ef | grep $NAME | egrep -v "grep|vi|more|pg" | wc -l)
+  RUNNING_PROCESSES=$(ps -ef | grep $NAME | wc -l)
+
+  echo RUNNING_PROCESSES : $RUNNING_PROCESSES
+done
