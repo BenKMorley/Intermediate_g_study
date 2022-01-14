c9ea128f09017f341f3eabfa2ac94065140046e1
BenKMorley <bkm1n18@soton.ac.uk>
2022-01-14 14:12:00 +0000

    Use script to see the effect changing the width has on the normality of the distributions

--- Local/Shapiro_Wilk_test_Binder.py
+++ Local/Shapiro_Wilk_test_Binder.py
@@ -1,12 +1,11 @@
 import numpy
 import sys
 import os
+import re
 from scipy.stats import shapiro
 import matplotlib.pyplot as plt
 
 # Import from the Core directory
-sys.path.append(os.getcwd() + '/..')
-sys.path.append(os.getcwd() + '/../Core')
 sys.path.append(os.getcwd())
 sys.path.append(os.getcwd() + '/Core')
 
@@ -14,38 +13,73 @@ from Core.model_definitions import *
 from Core.bayesian_functions import *
 from Core.parameters import param_dict, seperator, h5data_dir
 
-for N in [2, 4]:
-    print(seperator)
-    print("RUNNING FOR OLD DATA SET")
-    print(f'N = {N}')
-    N_s = [N]
-    Bbar_s = param_dict[N]["Bbar_list"]
-    g_s = param_dict[N]["g_s"]
-    L_s = param_dict[N]["L_s"]
-    Bbar_s = [0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59]
-    h5_data_file = f"{h5data_dir}Bindercrossings.h5"
-    h5_data_file2 = f"{h5data_dir}Bindercrossings_new_old_bounds.h5"
 
-    with h5py.File(h5_data_file, 'r') as f:
-        with h5py.File(h5_data_file2, 'r') as f2:
-            for g in g_s:
-                p_s = []
+def count_non_normal(filename, N):
+    total = 0
+    p0_05 = 0
+    p0_01 = 0
+    p0_001 = 0
+    p0_0001 = 0
+    p0_00000000001 = 0
 
-                for L in L_s:
-                    for Bbar in Bbar_s:
-                        success = True
-                        try:
-                            data = f[f'N={N}'][f'g={g:.2f}'][f'L={L}'][f'Bbar={Bbar:.3f}']['bs_bins'][()]
-                            data2 = f2[f'N={N}'][f'g={g:.2f}'][f'L={L}'][f'Bbar={Bbar:.3f}']['bs_bins'][()]
+    with h5py.File(filename, 'r') as f:
+        # We only want to analyse a single N
+        assert f'N={N}' in f.keys(), "The data file needs to contain data for this N"
 
-                        except Exception:
-                            success = False
+        data = f[f'N={N}']
+        data_points = 0
 
-                        if success:
-                            W, p = shapiro(data)
-                            if p < 10 ** -5:
-                                bins = plt.hist(data, label='Old Data', bins=20)
-                                plt.hist(data2, label='New Data', bins=bins[1])
-                                plt.title(f"g = {g}, L = {L}, Bbar = {Bbar}: p = {p}")
-                                plt.legend()
-                                plt.show()
+        for g_key in data.keys():
+            if len(re.findall(r'g=\d+\.\d+', g_key)) != 0:
+                g = float(re.findall(r'\d+\.\d+', g_key)[0])
+                g_data = data[g_key]
+
+            else:
+                print(f'Invalid g key: {g_key}')
+                continue
+
+            for L_key in g_data.keys():
+                if len(re.findall(r'L=\d+', L_key)) != 0:
+                    L = int(re.findall(r'\d+', L_key)[0])
+                    L_data = g_data[L_key]
+
+                else:
+                    print(f'Invalid L key: {L_key}')
+                    continue
+
+                for B_key in L_data.keys():
+                    if len(re.findall(r'Bbar=\d+.\d+', B_key)) != 0:
+                        B = float(re.findall(r'\d+.\d+', B_key)[0])
+                        B_data = L_data[B_key]
+                        data_points += 1
+
+                    else:
+                        print(f'Invalid L key: {B_key}')
+                        continue
+
+                    # Flag any samples that are zeros to avoid a singular matrix
+                    if numpy.sum(numpy.abs(B_data['bs_bins'])) == 0:
+                        print(f"Samples full of zeros for g = {g}, L = {L}, Bbar = {B}")
+                        continue
+
+                    samples = B_data['bs_bins'][()]
+                    W, p = shapiro(samples)
+
+                    total += 1
+
+                    if p < 0.05:
+                        p0_05 += 1
+                    if p < 0.01:
+                        p0_01 += 1
+                    if p < 0.001:
+                        p0_001 += 1
+                    if p < 0.0001:
+                        p0_0001 += 1
+                    if p < 0.00000000001:
+                        p0_00000000001 += 1
+
+    return p0_05, p0_01, p0_001, p0_0001, p0_00000000001
+
+
+for width in numpy.arange(10) / 10:
+    print(count_non_normal(f'h5data/width/width_{width:.1f}.h5', 2))
